f17a5a3a2be56475b2d89f08cd4ed3855e6600ba
curious <vishal238128@gmail.com>
2023-08-02 16:55:06 +0530

    middleware created, code added in component

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import About from "./pages/About"
+import PageNotFound from "./pages/PageNotFound";
+import Register from "./pages/Auth/Register";
+
+function App() {
+  return (
+    <>
+      <>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/policy' element={<Policy/>} />
+          <Route path='*' element={<PageNotFound/>} />
+        </Routes>
+      </>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Layout/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h4 className='text-center'>All Rights Reserved &copy; Dev</h4>
+      <p className="text-center">
+        <Link to='/about'>About</Link> |
+        <Link to='/contact'>Contact</Link> |
+        <Link to='/policy'>Policy</Link>
+      </p>
+    </div>
+  )
+}
+
+export default Footer
